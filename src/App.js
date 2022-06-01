5bd9d2bf81f815b012ca32daf4b13fbf62960f57
James <jaymz69@yahoo.com>
2022-05-31 18:02:05 -0700

    completed.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,70 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const api = {
   key: "bd8ce47b602572508e5b4bac2f9ade6d",
-  base: "https://api.openweathermap.org/data/2.5"
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
-  const [ lat, setLat ] = useState([]);
-  const [ long, setLong ] = useState([]);
 
+  const [ query, setQuery ] = useState('');
+  const [ weather, setWeather ] = useState({});
 
-  // useEffect(() => {
-  //   navigator.geolocation.getCurrentPosition(function (position) {
-  //     setLat(position.coords.latitude);
-  //     setLong(position.coords.longitude);
-  //   });
+  const search = e => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result)
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
 
-  //   console.log("Latitude is::: ", lat);
-  //   console.log('Longitude is >>>>>', long);
-  // }, [ lat, long ]);
+  const dateBuilder = (d) => {
+    const months = [ "January", "February", "March", "April", "May", "June", "July", "August",
+      "September", "October", "November", "December" ];
+    const days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
 
+    const day = days[ d.getDay() ];
+    const date = d.getDate();
+    const month = months[ d.getMonth() ];
+    const year = d.getFullYear();
 
+    return `${day} ${date} ${month} ${year}`;
+  }
 
 
 
   return (
-    <div className="app"> 
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 60) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="app__searchBox">
           <input
             type="text"
             className="app__searchBar"
             placeholder='Search...'
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="app__locationBox">
+              <div className="app__location">{weather.name}, {weather.sys.country}</div>
+              <div className="app__locationDate">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="app__weatherBox">
+              <div className="app__weatherTemp">
+                {Math.round(weather.main.temp)}°F
+              </div>
+              <div className="app__weather">{weather.weather[ 0 ].main}</div>
+            </div>
+          </div>
+        ) : ('')}
       </main>
     </div>
   );
